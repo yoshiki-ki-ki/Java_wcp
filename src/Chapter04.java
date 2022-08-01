eb2c551cc0fb92fd852d73e225e5aa3496398234
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-08-01 23:55:50 +0900

    [Chapter04]

---
+++ src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String My_name = "よしき";
+		System.out.println(My_name);
+		
+		My_name = "にしむら";
+		System.out.println(My_name);
+		
+		final int Lucky_number = 7;
+		System.out.println(Lucky_number);
+	}
+
+}
