ce5478e0bee7dbba0abb509a457adc60153ff7d3
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-08-02 18:47:09 +0900

    [Chapter08]

---
+++ src/continue01.java
@@ -0,0 +1,12 @@
+
+public class continue01 {
+	public static void main(String[] args) {
+		for (int count = 0; count < 5; count++) {
+			if (count == 2) {
+				// count が2の場合、この後の処理をスキップ。3以降が続けられる。
+				continue;
+			}
+			System.out.println("Continue01 = " + count);
+		}
+	}
+}
